d65e657c9b1305f65617cfdbaa406c4e8aef308e
Vahan <vahan@nanumyan.com>
2014-07-10 16:30:31 +0200

    preyView

--- web_app_v2/js/preyView.js
+++ web_app_v2/js/preyView.js
@@ -1,46 +1,4 @@
-
-preyData = { 'colors': ['#f8f9d1',
-                          '#574b52',
-                          '#8b6669',
-                          '#a4a95d',
-                          '#787a4d',
-                          '#e7eda5',
-                          '#cdd57a',
-                          '#c5c0a3',
-                          '#162442',
-                          '#af9184'],
-  'descr': 'Rethinking Vintage: Are the 80\u2032s and 90\u2032s fashion vintage?',
-  'hunterid': 22523,
-  'id': 128282,
-  'lat': null,
-  'link': 'http://www.wendybendoni.com/rethinking-vintage-80s-90s-fashion-vintage/',
-  'lng': null,
-  'platform': 'web',
-  'semanticClass': 'Warm Aesthetic',
-  'semantics': {'Cold Aesthetic': 0.0031608677324910434,
-              'Dark Sensitive Aesthetic': 0.14246000942207279,
-              'Dark Stimulating': 0.0010012281826534484,
-              'Easing Original': 0.00033568301050734372,
-              'Energetic Inspiring Original': 0.0010686049424429526,
-              'Energetic Original': 0.0,
-              'Impressive Structural Smart': 0.0014978186066103975,
-              'Joyous Flashy Aesthetic': 0.0,
-              'Joyous Inspiring Smart': 0.0,
-              'Lively Aesthetic': 0.0015230464829177996,
-              'Lively Original': 0.0,
-              'Luminous Energetic Aesthetic': 0.0,
-              'Luminous Natural Smart': 0.00047640499442059967,
-              'Luminous Stimulating': 0.0,
-              'Powerful Aesthetic': 0.0,
-              'Sensitive Energetic Original': 0.0,
-              'Sensitive Original': 0.00036871942897257636,
-              'Serene Structural Smart': 0.0,
-              'Soothing Aesthetic': 0.0,
-              'Warm Aesthetic': 0.84810761719691108
-               },
-  'tags': ['fashion', 'vintage', '80s', '90s']
-}
- 
+var colors = ["#66c2a5","#fc8d62","#8da0cb","#e78ac3","#a6d854","#ffd92f","#e5c494","#b3b3b3"]
 
 //d3.json(preyData, function(data){
 //    console.log('preyData', data)
@@ -49,30 +7,95 @@ preyData = { 'colors': ['#f8f9d1',
 ///////////////////////////////
 
 function showSemantics(preyData){
+    
+    var margin = {top: 20, right: 15, bottom: 20, left: 15};
+    var width = 300 - margin.left - margin.right,
+        height = 200 - margin.top - margin.bottom;
 
+    
     var len = 20 //preyData.length,
     var semantics = []
     sems = Object.keys(preyData.semantics)
     
     for ( var i=0; i < len; i+=1 ) {
-        semantics.push( {'semantic': sems[ i ], 'value': preyData.semantics[sems[ i ]]}  );   
+        sem = sems[ i ].split(' ')
+        
+        semantics.push( {'semantic': sem[sem.length - 1], 
+                         'subsemantic': sem.slice(0,-1).join(' '), 
+                         'value': preyData.semantics[sems[ i ]]}  );   
     }
 
+    var semanticsNest = d3.nest()
+        .key(function(d) { return d.semantic; })
+        .sortKeys(d3.ascending)
+        .entries(semantics);
     
-    console.log('preyData', semantics)
+    //console.log('preyData', semanticsNest)
     
-    svgPreyView = d3.select('#preySemantics svg')
+    var svgPreyView = d3.select('#preySemantics svg')
+    
+    svgPreyView.selectAll('g')
+        .remove()
+    
+    svgPreyView = svgPreyView.append('g')
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    
+    var bar = svgPreyView.append('g')
+        .selectAll('g')
+        .data(semanticsNest)
+        .enter()
         .append('g')
+        .attr('transform', function(d,i){ return 'translate(0,'+20*i+')'; } )
     
-    svgPreyView.selectAll('rect')
-        .data(semantics)
+    bar.append('text')
+        //.style('alignment-baseline', 'central')
+        .attr('y', 10)
+        .text(function(d) {
+            return d.key;
+        })
+    
+    bar.append('rect')
+        .style('fill', 'white')
+        .attr('opacity', 0.3)
+        .attr('x', 80)
+        .attr('height', 9)
+        .attr('width', 200)
+    
+    bar.append('rect')
+        .style('fill', function(d,i){ return colors[i]; })
+        .attr('x', 80)
+        //.attr('y', function(d,i){ return 10*i; })
+        .attr('height', 9)
+        .attr('width', 0)
+        .transition()
+        .duration(1000)
+        .attr('width', function(d,i){ 
+            var score = 0
+            for (var sub in d.values) {
+                score += d.values[sub].value
+            }
+            return 200*score;
+        })
+
+    ////////////
+    
+    var colorRecs = svgPreyView.append('g')
+        .attr('transform', 'translate(5,100)')
+        
+    colorRecs.selectAll('rect')
+        .data(preyData.colors)
         .enter()
         .append('rect')
-        .style('fill', 'red')
-        .attr('x', 0)
-        .attr('y', function(d,i){ return 10*i;})
-        .attr('height', 9)
-        .attr('width', function(d,i){ return 10*d;})
-
+        .attr('width', 23)
+        .attr('height', 23)
+        .attr('x', function(d,i){ return 28*i; })
+        .attr('stroke', 'white')
+        .attr('stroke-width', 1.5)
+        .attr('stroke-opacity', .8)
+        .attr('fill', function(d,i){ return d; })
+    
+    
 }
 
